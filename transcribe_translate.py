7150b92ae898e008c92a32c18e39cc9562142d0d
Devendra <tambatd@outlook.com>
2023-05-14 03:22:27 -0400

    init

---
+++ transcribe_translate.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+import pyaudio
+import openai
+
+def query_question(question):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=question,
+        max_tokens=100,
+        n=1,
+        stop=None
+    )
+    answer = response.choices[0].text.strip()
+    return answer
+
+def main():
+    init_rec = sr.Recognizer()
+    openai.api_key = '#'
+    with sr.Microphone() as source:
+        while True:
+            try:
+                audio_data = init_rec.record(source, duration=5)
+                text = init_rec.recognize_google(audio_data)
+                print(text)
+                try:
+                    answer = query_question(
+                        "You are hyper-fluent in Japanese, translate the following English sentence into Japanese: " + text)
+                    print(answer)
+                except:
+                    print("Chat-GPT Error")
+            except:
+                print("Audio Listener Error")
+
+
+if __name__ == "__main__":
+    main()
+   
